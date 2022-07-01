5a5a321412e1b51f4796bc625e2be2d8bf43b907
victor-cleber <victor.cleber@outlook.com>
2022-07-01 23:33:05 +1200

    Configure user functionalities

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('create/', views.create, name='create'),
+    path('validate_login/', views.create, name='validate_login'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-# Create your views here.
-
 def login(request):
-    return HttpResponse('login')
+    return render(request, 'login.html')
 
 
 def create(request):
     return render(request, 'create.html')
+
+
+def validate_login(request):
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    return HttpResponse(f"{email}{password}")
