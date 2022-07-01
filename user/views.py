eddea44c5d0e70d06e0e48135f584659b3b0e3ec
victor-cleber <victor.cleber@outlook.com>
2022-07-01 19:26:17 +1200

    Add user front and backend

---
+++ user/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def login(request):
+    return HttpResponse('login')
+
+
+def create(request):
+    return render(request, 'create.html')
